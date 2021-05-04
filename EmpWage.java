e9b16748d5da010e14b3e1d0782d45ac778b3e97
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-04 19:33:12 +0530

    Check employee is present or abscent

--- EmpWage.java
+++ EmpWage.java
@@ -4,6 +4,18 @@ public class EmpWage
 	public static void main(String[] args)
 	{
 		System.out.println("welcome");
+		//UC1
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random() *10 ) %2;
+		if(empCheck==IS_FULL_TIME)
+		{
+			System.out.println("emp is present");
+		}
+		else
+		{
+			System.out.println("employee is abscent");
+		}
+		
 		
 	}
 
