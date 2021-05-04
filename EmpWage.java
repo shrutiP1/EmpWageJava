1dabacd4b137d9210b6fa904df20c390bc5fb8ff
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-04 19:46:43 +0530

    calculate salary

--- EmpWage.java
+++ EmpWage.java
@@ -16,6 +16,22 @@ public class EmpWage
 			System.out.println("employee is abscent");
 		}
 		
+		//UC2
+	    int empWage=20;
+	    int empHr=8;
+	    int salary=0;
+	    if(empCheck==IS_FULL_TIME)
+	    {
+	    	salary=empWage*empHr;
+	    	System.out.println("salary is " +salary);
+	    	
+	    }
+	    else
+	    {
+	    	System.out.println("employee is abscent");
+	    }
+	    
+		
 		
 	}
 
