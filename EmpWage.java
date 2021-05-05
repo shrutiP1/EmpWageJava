8ca13756b0a982b0c1a6fea3fc9f0bbc20282a27
shruti raju patil <shrutipatil13798@gmail.com>
2021-05-06 00:05:03 +0530

    Welcome

--- EmpWage.java
+++ EmpWage.java
@@ -17,41 +17,41 @@ public class EmpWage
 		}
 		
 		//UC2
-	    int empWage=20;
-	    int empHr=8;
-	    int salary=0;
-	    if(empCheck==IS_FULL_TIME)
-	    {
-	    	salary=empWage*empHr;
-	    	System.out.println("salary is " +salary);
+	       int empWage=20;
+	       int empHr=8;
+	       int salary=0;
+	       if(empCheck==IS_FULL_TIME)
+	       {
+	    	  salary=empWage*empHr;
+	    	  System.out.println("salary is " +salary);
 	    	
-	    }
-	    else
-	    {
-	    	System.out.println("employee is abscent");
-	    }
+	       }
+	       else
+	       {
+	    	   System.out.println("employee is abscent");
+	       }
 	    
-		//UC3
+	      //UC3
 	    
-	    double empCheck1=Math.round(Math.random()*10)%3;
-	    int IS_PART_TIME=2;
-	    if (empCheck1==IS_FULL_TIME)
-	    {
-	    	empHr=8;
-	    }
-	    else if(empCheck1==IS_PART_TIME)
-	    {
-	    	empHr=4;
-	    }
-	    else
-	    {
+	      double empCheck1=Math.round(Math.random()*10)%3;
+	      int IS_PART_TIME=2;
+	      if (empCheck1==IS_FULL_TIME)
+	      {
+	    	  empHr=8;
+	      }
+	     else if(empCheck1==IS_PART_TIME)
+	      {
+	    	 empHr=4;
+	      }
+	     else
+	     {
 	    	empHr=0;
-	    	System.out.println("employee is abscent");
-	    }
+	    	System.out.println("employee is absent");
+	     }
 		salary=empWage*empHr;
 		System.out.println("salary depending on part time full time is "+salary);
 		
-		//UC4
+	     //UC4
 		int empCheck2=(int)empCheck1;
 		switch( empCheck2 )
 		{
@@ -69,7 +69,7 @@ public class EmpWage
 		
 		System.out.println("salary of employee using switch case "+(empHr*empWage));
 		
-		//UC5
+	       //UC5
 		int totalsalary =0;
 		salary=0;
 		for(int i =1 ; i<=20 ; i++)
@@ -96,15 +96,15 @@ public class EmpWage
 		System.out.println("total salary for 20 days "+totalsalary);
 		
 		
-	//UC6
-	int totalWorkingHours=0;
-	int totalDays=0;
+	    //UC6
+	      int totalWorkingHours=0;
+	      int totalDays=0;
 	
-	while(totalWorkingHours<=100 && totalDays<=20)
-	{
-	  totalDays++;
-	  double empCheck3=Math.round(Math.random()*10)%3;
-		 if (empCheck3==IS_FULL_TIME)
+	     while(totalWorkingHours<=100 && totalDays<=20)
+	     {
+	             totalDays++;
+	             double empCheck3=Math.round(Math.random()*10)%3;
+		     if (empCheck3==IS_FULL_TIME)
 		    {
 		    	empHr=8;
 		    }
@@ -120,7 +120,7 @@ public class EmpWage
 		   salary=empHr*empWage;
 		   totalsalary=salary+totalsalary;
 		   totalWorkingHours=totalWorkingHours+empHr;
-	}
-	System.out.println("total wages of employee in 100 empHr and 20 days "+totalsalary);
+	      }
+	       System.out.println("total wages of employee in 100 empHr and 20 days "+totalsalary);
 	}
 }
